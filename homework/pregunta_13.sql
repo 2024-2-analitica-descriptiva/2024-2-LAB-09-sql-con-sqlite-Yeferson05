7936a31bae1f54bf2a4a23b9b1e5d3101efbc558
Yeferson05 <yaguilar@unal.edu.co>
2025-01-02 21:31:26 -0500

    Lab9

--- homework/pregunta_13.sql
+++ homework/pregunta_13.sql
@@ -43,7 +43,7 @@
 -- 
 --  >>> Escriba su codigo a partir de este punto <<<
 --  
-SELECT K0, ROUND(CAST(AVG(c12) AS NUMERIC), 2) AS "avg(c12)"
+SELECT K0, CAST(FLOOR(AVG(c12) * 100 + 0.5) AS INT) / 100.0 AS "avg(c12)"
 FROM tbl1
 WHERE c13 > 400
 GROUP BY K0
